ff445d827a3ccd38a8709006b89b8c27ad6ae9ae
Aldo <Hola564@hotmail.es>
2020-08-25 06:28:01 -0400

    Función GitAdd
    Se implementó la función GitAdd.

--- Repositorio.java
+++ Repositorio.java
@@ -1,5 +1,7 @@
+import java.awt.desktop.SystemSleepEvent;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Repositorio
@@ -42,42 +44,47 @@ public class Repositorio
     {
 
         int ver = 0;
-        int respuesta = -1;
+        String respuesta = "";
 
         while (ver == 0)
         {
             mostrar_menu();
-            respuesta = sc.nextInt();
+
+            respuesta = sc.nextLine();
+
             switch (respuesta)
             {
-                case 1:
+                case "1":
                     gitAdd();
                     break;
 
-                case 2:
+                case "2":
                     gitCommit();
                     break;
 
-                case 3:
+                case "3":
                     gitPull();
                     break;
 
-                case 4:
+                case "4":
                     gitPush();
                     break;
 
-                case 5:
+                case "5":
                     gitStatus();
                     break;
 
-                case 6:
+                case "6":
                     nuevoArchivo();
                     break;
 
-                case 7:
+                case "7":
                     ver = 1;
                     System.out.println("Saliendo...");
                     break;
+
+                default:
+                    System.out.println("Ingrese una opcion valida");
             }
 
         }
@@ -102,11 +109,92 @@ public class Repositorio
     }
 
     // Función que añade un archivo (o varios) desde el Work Space al Index.
+    // Entrada: no tiene.
+    // Salida: no tiene.
     public void gitAdd()
     {
-        System.out.println("Funcion gitAdd");
+        //System.out.println("Funcion gitAdd");
+
+        int ver = 0;
+        int cantidad = 0;
+        String nombre_Aux;
+        String cant_Aux;
+
+        // Cantidad de archivos a agregar
+        while (ver == 0)
+        {
+
+            try {
+                System.out.println("#####################################################");
+                System.out.print("# Ingrese el numero de archivos que desea agregar: ");
+                cant_Aux = sc.nextLine();
+                cantidad = Integer.parseInt(cant_Aux);
+                System.out.println("#####################################################");
+                ver = 1;
+
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println("# Ingrese un numero valido.");
+            }
+
+        }
+
+        // Obteniendo los nombres de esos archivos
+        ArrayList<String> nombres = new ArrayList<>();
+        int i = 0;
+        while (i < cantidad)
+        {
+            try {
+                System.out.println("#####################################################");
+                System.out.print("# Ingrese el nombre del archivo que desea agregar (con extension correspondiente): ");
+                nombre_Aux = sc.nextLine();
+                System.out.println("#####################################################");
+                nombres.add(nombre_Aux);
+                i++;
+            }
+            catch (Exception e)
+            {
+                System.out.println("# Ingrese un nombre valido.");
+            }
+        }
+
+        // Rescatando los indices que calzen
+        ArrayList<Integer> indices = comparar_WS_nombres(nombres);
+
+        for (int indice : indices) {
+            this.zona.index.archivos.add(this.zona.workSpace.archivos.get(indice));
+        }
+
+        // En caso de que ninguno calce se dara aviso
+        if (indices.size() == 0)
+        {
+            System.out.println("# No se encontro ningun archivo mencionado en los archivos del Work Space.\n# Porfavor asegurese de agregar los archivos en primera instancia.");
+        }
 
 
+    }
+
+    private ArrayList<Integer> comparar_WS_nombres(ArrayList<String> nombres)
+    {
+        int largo_Nombres = nombres.size();
+        int largo_WS = this.zona.workSpace.archivos.size();
+
+        ArrayList<Integer> indices = new ArrayList<>();
+
+        for (int i = 0; i < largo_Nombres; i++)
+        {
+            for (int k = 0; k < largo_WS; k++)
+            {
+                if ((this.zona.workSpace.archivos.get(k).nombre.equals(nombres.get(i))))
+                {
+                    indices.add(k);
+                }
+            }
+        }
+
+
+        return indices;
     }
 
     // Función que genera un commit desde Index a Local Repository con los archivos en Index y el mensaje pedido.
